573c46cb1cd7dd7c42fd4b8bfd59bbbbdd6aa3f8
Lukas Lieb <lukas.lieb@axonivy.com>
2025-07-23 12:14:19 +0200

    XIVY-16606 Adapt basic dialog changes

--- packages/dataclass-editor/src/master/AddFieldDialog.tsx
+++ packages/dataclass-editor/src/master/AddFieldDialog.tsx
@@ -1,21 +1,23 @@
 import type { DataClass, Field } from '@axonivy/dataclass-editor-protocol';
 import {
   addRow,
-  BasicDialog,
+  BasicDialogContent,
   BasicField,
   Button,
+  Dialog,
+  DialogContent,
   DialogTrigger,
-  Flex,
   hotkeyText,
   Input,
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-  useHotkeyLocalScopes,
+  useDialogHotkeys,
   useHotkeys,
   type MessageData
 } from '@axonivy/ui-components';
+import { IvyIcons } from '@axonivy/ui-icons';
 import { type Table } from '@tanstack/react-table';
 import { useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -24,44 +26,40 @@ import { isEntity } from '../data/dataclass-utils';
 import { BROWSER_BTN_ID, InputFieldWithTypeBrowser } from '../detail/field/InputFieldWithTypeBrowser';
 import { useKnownHotkeys } from '../utils/useKnownHotkeys';
 
-export const validateFieldName = (name: string, dataClass: DataClass) => {
-  if (name.trim() === '') {
-    return toErrorMessage('Name cannot be empty.');
-  }
-  if (dataClass.fields.some(field => field.name === name)) {
-    return toErrorMessage('Name is already taken.');
-  }
-  return;
-};
-
-export const validateFieldType = (type: string) => {
-  if (type.trim() === '') {
-    return toErrorMessage('Type cannot be empty.');
-  }
-  return;
-};
-
-const toErrorMessage = (message: string): MessageData => {
-  return { message: message, variant: 'error' };
-};
+const DIALOG_HOTKEY_IDS = ['addFieldDialog'];
 
 type AddFieldDialogProps = { table: Table<Field>; children: React.ReactNode };
 
 export const AddFieldDialog = ({ table, children }: AddFieldDialogProps) => {
+  const { open, onOpenChange } = useDialogHotkeys(DIALOG_HOTKEY_IDS);
+  const { addAttr: shortcut } = useKnownHotkeys();
+  useHotkeys(shortcut.hotkey, () => onOpenChange(true), { scopes: ['global'], keyup: true, enabled: !open });
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <DialogTrigger asChild>{children}</DialogTrigger>
+          </TooltipTrigger>
+          <TooltipContent>{shortcut.label}</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <DialogContent>
+        <AddFieldDialogContent table={table} closeDialog={() => onOpenChange(false)} />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export const AddFieldDialogContent = ({ table, closeDialog }: { table: Table<Field>; closeDialog: () => void }) => {
   const nameInputRef = useRef<HTMLInputElement>(null);
   const { dataClass, setDataClass, setSelectedField } = useAppContext();
-  const { activateLocalScopes, restoreLocalScopes } = useHotkeyLocalScopes(['addFieldDialog']);
 
-  const [name, setName] = useState('');
-  const [type, setType] = useState('');
+  const [name, setName] = useState('newAttribute');
+  const [type, setType] = useState('String');
 
   const nameValidationMessage = useMemo(() => validateFieldName(name, dataClass), [name, dataClass]);
   const typeValidationMessage = useMemo(() => validateFieldType(type), [type]);
-
-  const initializeAddFieldDialog = () => {
-    setName('newAttribute');
-    setType('String');
-  };
 
   const addField = (e: React.MouseEvent<HTMLButtonElement, MouseEvent> | KeyboardEvent) => {
     const newField: Field = {
@@ -84,7 +82,7 @@ export const AddFieldDialog = ({ table, children }: AddFieldDialogProps) => {
     });
 
     if (!e.ctrlKey && !e.metaKey) {
-      onOpenChange(false);
+      closeDialog();
     } else {
       setName('');
       nameInputRef.current?.focus();
@@ -92,18 +90,6 @@ export const AddFieldDialog = ({ table, children }: AddFieldDialogProps) => {
   };
 
   const allInputsValid = () => !nameValidationMessage && !typeValidationMessage;
-  const [open, setOpen] = useState(false);
-  const onOpenChange = (open: boolean) => {
-    setOpen(open);
-    if (open) {
-      initializeAddFieldDialog();
-      activateLocalScopes();
-    } else {
-      restoreLocalScopes();
-    }
-  };
-  const { addAttr: shortcut } = useKnownHotkeys();
-  useHotkeys(shortcut.hotkey, () => onOpenChange(true), { scopes: ['global'], keyup: true, enabled: !open });
   const enter = useHotkeys(
     ['Enter', 'mod+Enter'],
     e => {
@@ -112,59 +98,66 @@ export const AddFieldDialog = ({ table, children }: AddFieldDialogProps) => {
       }
       addField(e);
     },
-    { scopes: ['addFieldDialog'], enabled: open, enableOnFormTags: true }
+    { scopes: DIALOG_HOTKEY_IDS, enableOnFormTags: true }
   );
   const { t } = useTranslation();
 
   return (
-    <BasicDialog
-      open={open}
-      onOpenChange={onOpenChange}
-      contentProps={{
-        title: t('dialog.addAttr.title'),
-        description: t('dialog.addAttr.desc'),
-        onCloseAutoFocus: e => e.preventDefault(),
-        buttonClose: (
-          <Button variant='outline' size='large'>
-            {t('common.label.cancel')}
-          </Button>
-        ),
-        buttonCustom: (
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant='primary'
-                  size='large'
-                  aria-label={t('dialog.addAttr.create')}
-                  disabled={!allInputsValid()}
-                  onClick={addField}
-                >
-                  {t('dialog.addAttr.create')}
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>{t('dialog.addAttr.createTooltip', { modifier: hotkeyText('mod') })}</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        )
-      }}
-      dialogTrigger={
+    <BasicDialogContent
+      title={t('dialog.addAttr.title')}
+      description={t('dialog.addAttr.desc')}
+      submit={
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger asChild>
-              <DialogTrigger asChild>{children}</DialogTrigger>
+              <Button
+                variant='primary'
+                size='large'
+                aria-label={t('dialog.addAttr.create')}
+                disabled={!allInputsValid()}
+                onClick={addField}
+                icon={IvyIcons.Plus}
+              >
+                {t('dialog.addAttr.create')}
+              </Button>
             </TooltipTrigger>
-            <TooltipContent>{shortcut.label}</TooltipContent>
+            <TooltipContent>{t('dialog.addAttr.createTooltip', { modifier: hotkeyText('mod') })}</TooltipContent>
           </Tooltip>
         </TooltipProvider>
       }
+      cancel={
+        <Button variant='outline' size='large'>
+          {t('common.label.cancel')}
+        </Button>
+      }
+      ref={enter}
+      tabIndex={-1}
     >
-      <Flex ref={enter} tabIndex={-1} direction='column' gap={2}>
-        <BasicField label={t('common.label.name')} message={nameValidationMessage} aria-label={t('common.label.name')}>
-          <Input ref={nameInputRef} value={name} onChange={event => setName(event.target.value)} />
-        </BasicField>
-        <InputFieldWithTypeBrowser value={type} message={typeValidationMessage} onChange={setType} />
-      </Flex>
-    </BasicDialog>
+      <BasicField label={t('common.label.name')} message={nameValidationMessage} aria-label={t('common.label.name')}>
+        <Input ref={nameInputRef} value={name} onChange={event => setName(event.target.value)} />
+      </BasicField>
+      <InputFieldWithTypeBrowser value={type} message={typeValidationMessage} onChange={setType} />
+    </BasicDialogContent>
   );
+};
+
+export const validateFieldName = (name: string, dataClass: DataClass) => {
+  if (name.trim() === '') {
+    return toErrorMessage('Name cannot be empty.');
+  }
+  if (dataClass.fields.some(field => field.name === name)) {
+    return toErrorMessage('Name is already taken.');
+  }
+  return;
+};
+
+export const validateFieldType = (type: string) => {
+  if (type.trim() === '') {
+    return toErrorMessage('Type cannot be empty.');
+  }
+  return;
+};
+
+const toErrorMessage = (message: string): MessageData => {
+  return { message: message, variant: 'error' };
 };
